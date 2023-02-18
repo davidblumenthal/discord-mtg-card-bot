9197e2703d94a9e1c7f220eba0236aa1c2feb16c
David Blumenthal <blumenthal@indeed.com>
2023-02-18 10:52:41 -0600

    Added getAllDataInPath

--- src/util/trie.test.ts
+++ src/util/trie.test.ts
@@ -1,27 +1,43 @@
-import {addData, getData, newNode, Node} from "./trie";
+import {makeTrie} from "./trie";
 
 describe('trie', () => {
-    let trie: Node;
+    let trie: ReturnType<typeof makeTrie>;
 
     beforeAll(() => {
-        trie = newNode();
-        addData(trie, ["the", "orb"], "this is the orb");
-        addData(trie, ["the", "orb", "of", "darkness"], "this is the orb of darkness");
+        trie = makeTrie();
+        trie.addData(["orb"], "this is the orb");
+        trie.addData(["orb", "of", "darkness"], "this is the orb of darkness");
+        trie.addData(["shining", "orb"], "this is the shining orb");
     });
 
-    it('should find the short path', () => {
-        expect(getData(trie, ["the", "orb"])).toBe("this is the orb");
+    describe('getData', () => {
+
+        it('should find the short path', () => {
+            expect(trie.getData(["the", "orb"])).toBe("this is the orb");
+        });
+
+        it('should find the long path', () => {
+            expect(trie.getData(["the", "orb", "of", "darkness"])).toBe("this is the orb of darkness");
+        });
+
+        it('should not blow up with an extra-long path', () => {
+            expect(trie.getData(["the", "orb", "of", "darkness", "with", "more"])).toBeUndefined();
+        });
+
+        it('should not blow up with an unknown path', () => {
+            expect(trie.getData(["should", "not", "be", "found"])).toBeUndefined();
+        });
     });
 
-    it('should find the long path', () => {
-        expect(getData(trie, ["the", "orb", "of", "darkness"])).toBe("this is the orb of darkness");
-    });
-
-    it('should not blow up with an extra-long path', () => {
-        expect(getData(trie, ["the", "orb", "of", "darkness", "with", "more"])).toBeUndefined();
-    });
-
-    it('should not blow up with an unknown path', () => {
-        expect(getData(trie, ["should", "not", "be", "found"])).toBeUndefined();
+    describe("getAllDataInPath", () => {
+        it("should return two cards", () => {
+            expect(trie.getAllDataInPath(
+                ["this","is","the","shining","orb","of","darkness","right"]
+            )).toStrictEqual([
+                "this is the shining orb",
+                "this is the orb",
+                "this is the orb of darkness"
+            ]);
+        })
     });
 });
--- src/util/trie.ts
+++ src/util/trie.ts
@@ -1,16 +1,15 @@
-export type Node = {
-    data : any;
+type Node = {
+    data ?: any;
     children : Record<string,Node>;
 };
 
-export const newNode = () : Node => {
+const newNode = () : Node => {
     return {
-        data : undefined,
         children : {}
     };
 };
 
-export function addData(node : Node, path : string[], data : any, pathIndex: number = 0) : void {
+function addData(node : Node, path : string[], data : any, pathIndex: number = 0) : void {
     if(!node.children[path[pathIndex]]){
         node.children[path[pathIndex]] = newNode();
     }
@@ -23,7 +22,7 @@ export function addData(node : Node, path : string[], data : any, pathIndex: num
     }
 }
 
-export function getData(node : Node, path : string[], pathIndex: number = 0) : any | undefined {
+function getData(node : Node, path : string[], pathIndex: number = 0) : any | undefined {
     if(pathIndex === path.length) {
         return node.data;
     }
@@ -32,4 +31,33 @@ export function getData(node : Node, path : string[], pathIndex: number = 0) : a
         return getData(childNode, path, pathIndex+1);
     }
     return undefined;
+}
+
+function getAllDataInPath(node: Node, path : string[]) : any[] {
+    const outputSet : Set<any> = new Set();
+
+    for (let startPathIndex = 0; startPathIndex<path.length; startPathIndex++) {
+        let currentNode = node;
+        for (let pathIndex = startPathIndex; pathIndex < path.length; pathIndex++) {
+            const word = path[pathIndex];
+            if ("data" in currentNode) {
+                outputSet.add(currentNode.data);
+            }
+            if (currentNode.children[word]) {
+                currentNode = currentNode.children[word];
+            } else {
+                break;
+            }
+        }
+    }
+    return [...outputSet];
+}
+
+export function makeTrie() {
+  const trie = newNode();
+  return {
+      addData: (path:string[], data:any) => addData(trie,path, data),
+      getData: (path:string[]) => getData(trie, path),
+      getAllDataInPath: (path:string[]) => getAllDataInPath(trie, path)
+  };
 }
